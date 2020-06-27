ca47e0ed05357d84e25e330f1168df336d933693
Rajeev Singh <callicoder@gmail.com>
2020-06-27 14:10:53 +0530

    PointerUtil

---
+++ pointerutil/pointerutil.go
@@ -0,0 +1,36 @@
+package pointerutil
+
+func NewPointerString(s string) *string {
+	if s == "" {
+		return nil
+	}
+	return &s
+}
+
+func NewPointerInt64(i int64) *int64 {
+	if i == 0 {
+		return nil
+	}
+	return &i
+}
+
+func NewPointerFloat64(f float64) *float64 {
+	if f == 0 {
+		return nil
+	}
+	return &f
+}
+
+func NewPointerBool(b bool) *bool {
+	if b == false {
+		return nil
+	}
+	return &b
+}
+
+func NewPointerTime(t time.Time) *time.Time {
+	if (t == time.Time{}) {
+		return nil
+	}
+	return &t
+}
